8457acc643b3746e464cea1a0187dba6f0bd9110
Camilo <yeisongamba97@gmail.com>
2026-03-26 17:11:40 -0500

    security: move firebase secrets to environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAv1Soy4j_dYXs0SkEpJw6yDvpsrsyrBI8",
-    authDomain: "healthsaas-c8a16.firebaseapp.com",
-    projectId: "healthsaas-c8a16",
-    storageBucket: "healthsaas-c8a16.firebasestorage.app",
-    messagingSenderId: "999115836994",
-    appId: "1:999115836994:web:4b04510b10483eb1e927db",
-    measurementId: "G-PSC3MK0Q97"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
